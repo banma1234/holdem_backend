9ac06fb7e897fc3a08b5330af9e98e80122c2497
chocoham <banma1234@gmail.com>
2024-03-24 23:46:25 +0900

    refactor: Player와 GameTable간의 역할 명확히 분리

--- src/game/GameTable.js
+++ src/game/GameTable.js
@@ -1,10 +1,30 @@
+import tableManager from "./TableManager";
 const holdem = require("holdem");
 const Player = require("./Player");
+const CurrentGame = require("./CurrentGame");
 
 class GameTable {
-  constructor(socketio, tableId) {
-    this.socketio = socketio;
+  constructor(tableSocket, gameSocket, tableId) {
+    this.tableSocket = tableSocket;
+    this.gameSocket = gameSocket;
     this.tableId = tableId;
-    this.players = [];
+
+    this.players = new Map();
+    this.currentGame = new CurrentGame(this.gameSocket);
+  }
+
+  addPlayer(user) {
+    const newPlayer = new Player(
+      tableManager.getTable(this.tableId),
+      this.currentGame,
+      user
+    );
+    this.players.set(user.email, newPlayer);
+  }
+
+  startGame() {
+    this.currentGame.joinPlayerToGame(this.players);
   }
 }
+
+module.exports = GameTable;
--- src/game/Player.js
+++ src/game/Player.js
@@ -2,56 +2,15 @@ const GameTable = require("./GameTable");
 const User = require("../model/User");
 
 class Player {
-  constructor(soketio, gameSocket) {
-    this.soketio = soketio;
-    this.gameSocket = gameSocket;
-    this.currentTable = undefined;
-    this.currentSit = undefined;
-    this.user = undefined;
+  constructor(currentTable, currentGame, user) {
+    this.currentTable = currentTable;
+    this.currentGame = currentGame;
+    this.user = user;
 
-    gameSocket.on("disconnect", this.disconnectFromTable);
-
-    gameSocket.on("leaveTalbe", this.disconnectFromTable);
-    gameSocket.on("joinTable", this.joinTable);
-    gameSocket.on("createTable", this.currentTable);
-    gameSocket.on("sitTable", this.sitTable);
-
-    /** 배팅은 나중에 */
-    // gameSocket.on("fold", this.fold);
-    // gameSocket.on("check", this.check);
-    // gameSocket.on("raise", this.raise);
-    // gameSocket.on("call", this.call);
-    // gameSocket.on("bet", this.bet);
-
-    // this.initChat();
+    // this.gameSocket.on("connect", (socket) => {
+    //   console.log(`플레이어 연결 : ${user.name} => ${socket.id}`);
+    // });
   }
 }
 
-// initChat = () => {
-//   this.gameSocket.on("videoCallTable", (data) => {
-//     console.log("call mode");
-
-//     this.soketio.to(data.userToCall).emit("callIncoming", {
-//       signal: data.signalData,
-//       from: data.from,
-//     });
-//   });
-
-//   this.gameSocket.on("acceptCall", (data) => {
-//     console.log("call accepted");
-//     this.socketio.to(data.to).emit("callAccepted", data.signal);
-//   });
-// };
-
-joinActiveTable = (data) => {
-  this.disconnectFromTable();
-  var { tableId } = data;
-
-  var tableRoom = this.socketio.sockets.adapter.roms.get(tableId);
-  var table = activeTables[tableId];
-
-  if (tableRoom === undefined || table === undefined) {
-    this.gameSocket.emit("status", "incorrect");
-    // 내용 보충 필요
-  }
-};
+module.exports = Player;
